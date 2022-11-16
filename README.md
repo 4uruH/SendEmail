0569e6974c2168af8974de794e9f40d2dde15184
4uruH <97702288+4uruH@users.noreply.github.com>
2022-11-16 22:13:29 +0300

    Emailsend Script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Script for sending several emails using python
+
+
+
+If you want to use a script, allow the email server to work with unwanted applications
--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+import smtplib
+import os
+from email.mime.text import MIMEText
 
+
+def sendmail(message):
+    sender = "youremail@gmail.com"
+    # можно напрямую записать пароль в кавычках, можно взят из переменой окружения
+    password = os.getenv("EMAIL_PASSWORD")
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+
+    try:
+        server.login(sender, password)
+        # блок отправки сообщений от кого, кому(тут может быть цикл с сотней адресов), само сообщение
+        msg = MIMEText(message)
+        msg["Subject"] = "Любая тема хоть кирилицей хоть латиницей"
+        server.sendmail(sender, sender, msg.as_string())
+
+        return "message send!"
+    except Exception as _ex:
+        return f"{_ex}\nCheck your login or password"
+
+
+def main():
+    message = input("Type your message!: ")
+    sendmail(message=message)
+
+
+if __name__ == "__main__":
+    main()
